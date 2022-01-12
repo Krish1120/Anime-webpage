cc6bd29fa5f57f46eb7783eb01edc19dc770896d
Krish1120 <48213650+Krish1120@users.noreply.github.com>
2022-01-12 19:05:30 +0530

    Basic Webpage

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const navbarslide = () => {
-    const togglebtn = document.querySelector('.togglebtn');
-    const navbar = document.querySelector('.navlinks');
-    const navlinks = document.querySelectorAll('.navlinks li')
+  const togglebtn = document.querySelector(".togglebtn");
+  const navbar = document.querySelector(".navlinks");
+  const navlinks = document.querySelectorAll(".navlinks li");
 
-    togglebtn.addEventListener('click', () =>{
-        navbar.classList.toggle('togglebtn-active');
+  togglebtn.addEventListener("click", () => {
+    navbar.classList.toggle("togglebtn-active");
 
-        navlinks.forEach((link, index) => {
-            if(link.style.animation){
-                link.style.animation = '';
-            }else{
-                link.style.animation = `navlinkFade 0.5s ease forwards ${index/4}s`
-            }
-        });
-        togglebtn.classList.toggle('toggle')
-
+    navlinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navlinkFade 0.5s ease forwards ${index / 4}s`;
+      }
     });
-}
+    togglebtn.classList.toggle("toggle");
+  });
+};
 navbarslide();
